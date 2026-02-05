8c494953099af2ca41f49b13c35db36138b528e4
Álvaro García Friebe <monisfr29@gmail.com>
2026-02-05 20:02:37 +0100

    Initial Commit

---
+++ src/main/java/com/caritrack/caritrack_api/CaritrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.caritrack.caritrack_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaritrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaritrackApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caritrack/caritrack_api/CaritrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caritrack.caritrack_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaritrackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
